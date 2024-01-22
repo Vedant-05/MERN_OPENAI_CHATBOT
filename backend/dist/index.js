46d04e0ea94c7124801b85fd50ee203aa9c24a31
Vedant Joshi <vedantj76@gmail.com>
2024-01-22 19:40:38 +0530

    Auth implemented for signup

---
+++ backend/dist/index.js
@@ -0,0 +1,14 @@
+import app from "./app.js";
+import { connectToDataBase } from "./db/connection.js";
+//connnections
+const PORT = process.env.PORT || 5000;
+connectToDataBase()
+    .then(() => {
+    app.listen(PORT, () => {
+        console.log("server open and connected to db 🙌");
+    });
+})
+    .catch((error) => {
+    console.log(error);
+});
+//# sourceMappingURL=index.js.map
---
+++ backend/src/utils/validators.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import { ValidationChain, body, validationResult } from "express-validator";
+
+export const validate = (validations: ValidationChain[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    for (let validation of validations) {
+      const error = await validation.run(req);
+      if (!error.isEmpty()) {
+        break;
+      }
+    }
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+
+    return res.status(422).json({ errors: errors.array() });
+  };
+};
+
+export const signupValidator = [
+  body("name").notEmpty().withMessage("Name is required"),
+  body("email").trim().isEmail().withMessage("Email is required"),
+  body("password")
+    .trim()
+    .isLength({ min: 6 })
+    .withMessage("Password should contain atleast 6 characters"),
+];
+
+
